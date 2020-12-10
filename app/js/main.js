0b540b86999bbae476c7b71bcb9f7f4b439cdb6b
graff.gorshin@gmail.com <graff.gorshin@gmail.com>
2020-12-10 23:14:18 +0200

    adaptive not complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,45 @@
 $(function () {
+
+   //for menu anchor
+   var StickyElement = function (node) {
+     var doc = $(document),
+       fixed = false,
+       anchor = node.find('.anchor'),
+       content = node.find('.menu');
+
+     var onScroll = function (e) {
+       var docTop = doc.scrollTop(),
+         anchorTop = anchor.offset().top;
+
+       console.log('scroll', docTop, anchorTop);
+       if (docTop > anchorTop) {
+         if (!fixed) {
+           anchor.height(content.outerHeight());
+           content.addClass('fixed');
+           fixed = true;
+         }
+       } else {
+         if (fixed) {
+           anchor.height(0);
+           content.removeClass('fixed');
+           fixed = false;
+         }
+       }
+     };
+
+     $(window).on('scroll', onScroll);
+   };
+
+   var demo = new StickyElement($('#fixed'));
+
+
+  
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000
+    // autoplay: true,
+    // autoplaySpeed: 3000
   });
   
   $('.filter__btn').on('click', function () {
@@ -12,7 +47,18 @@ $(function () {
     $(this).addClass('active');
   });
   
-  var mixer1 = mixitup('.production');
-  var mixer2 = mixitup('.design');
+  // var mixer1 = mixitup('.production');
+  // var mixer2 = mixitup('.design');
+
+  document.onkeydown = function (e) {
+    if (e.keyCode === 13) {
+      document.activeElement.click();
+    }
+  };
+
+  // $('.menu__btn, .menu a').on('click', function () {
+  //   $('.header__top').toggleClass('header__top--active');
+  // });
+
 
 });
