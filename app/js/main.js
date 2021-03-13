f440a9987c3d84e5454c6f2be061ef2e0dbab9bc
SERHII-YAKYMENKO <graff.gorshin@gmail.com>
2021-03-13 23:12:30 +0200

    during

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,34 @@
 $(function () {
 
+$('.details__item').on('click', function (e) {
+  e.preventDefault();
+  $('.details__item').removeClass('details__item--active');
+  $(this).addClass('details__item--active');
+
+  $('.details__content-tab').removeClass('details__content-tab--active');
+  $($(this).attr('href')).addClass('details__content-tab--active');
+  
+});
+
+  $('.details-slide__samll').slick({
+    asNavFor: '.details-slide__normal',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+
+  $('.details-slide__normal').slick({
+    asNavFor: '.details-slide__samll',
+    focusOnSelect: true,
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+  $('input, select').styler();
+
   $('.price__input').ionRangeSlider({
     type: "double",
 
@@ -25,6 +54,15 @@ $(function () {
   $(".star-line").rateYo({
     starWidth: "18px",
     spacing: "5px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    "starSvg": '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" class="svg - inline--fa fa - star fa - w - 18 " role="img" xmlns="http: //www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>'
+  });
+
+  $(".star-details").rateYo({
+    starWidth: "18px",
+    spacing: "13px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     readOnly: true,
@@ -66,7 +104,7 @@ $(function () {
   $('.top-slider__items').slick({
     dots: true,
     arrows: false,
-    fade: true,
+    // fade: true,
     // respondTo: min,
     // centerMode: true,
     // variableWidth: true,
